fa804b1d584bd63bff938a523c480f5385f48283
User22090 <arjitawasthi808@gmail.com>
2026-08-30 22:13:16 +0530

    Compiler update

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 import json
 from lark import Lark
 from pathlib import Path
-from .Transformer import JockyTransformer
+from Transformer import JockyTransformer
 
 BASE_DIR = Path(__file__).resolve().parent
 with open(BASE_DIR / "grammar.lark","r") as file:
@@ -18,3 +18,5 @@ def parse_command(command):
     transformer = JockyTransformer()
 
     return transformer.transform(tree)
+
+print(parse_command("SYSTEM INFO"))
